bc3d12b80690c3d266787828ab28436913969ed1
oliinykroman <oliinykroman@gmail.com>
2025-05-17 19:04:49 +0300

    updated

--- contact/contact-process.php
+++ contact/contact-process.php
@@ -1,55 +1,14 @@
 <?php
-$address = "oliinykroman@gmail.com";
-if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$to = "oliinykroman@gmail.com";
+		$subject = "Appointment for consultation";
+		$message = "Name: " . $_POST["name"] . "\nPhone: " . $_POST["name"] . "\nEmail: " . $_POST["email"];
+		$headers = "From: noreply@yourdomain.com";
 
-$error = false;
-$fields = array( 'name', 'email','phone' );
-
-foreach ( $fields as $field ) {
-	if ( empty($_POST[$field]) || trim($_POST[$field]) == '' )
-		$error = true;
-}
-
-if ( !$error ) {
-
-	$name = stripslashes($_POST['name']);
-	$email = trim($_POST['email']);	
-	// $message = stripslashes($_POST['message']);
-	$phone = trim($_POST['phone']);
-	// $subject = trim($_POST['subject']);
-
-	$e_subject = 'You\'ve been contacted by ' . $name . '.';
-	
-
-	// Configuration option.
-	// You can change this if you feel that you need to.
-	// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
-
-	$e_body = "You have been contacted by: $name" . PHP_EOL . PHP_EOL;
-	$e_reply = "E-mail: $email" . PHP_EOL . PHP_EOL;
-	// $e_content = "Message:\r\n$message \r\n Phone: $phone \r\n Subject: $subject" . PHP_EOL;
-	$e_content = "Phone: $phone" . PHP_EOL;
-	
-
-	$msg = wordwrap( $e_body . $e_reply , 70 );
-
-	$headers = "From: $email" . PHP_EOL;
-	$headers .= "Reply-To: $email" . PHP_EOL;
-	$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-	$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
-
-	if(mail($address, $msg, $headers, $e_content  )) {
-
-		// Email has sent successfully, echo a success page.
-	
-		echo 'Success';
-
-	} else {
-
-		echo 'ERROR!';
-
+		if (mail($to, $subject, $message, $headers)) {
+			echo "success";
+		} else {
+			echo "error sending";
+		}
 	}
-
-}
-
 ?>
